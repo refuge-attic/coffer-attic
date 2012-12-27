0fa053cdcf72262ca8c8a61cd4af27708483b7d9
Nicolas R Dufour <nicolas.dufour@nemoworld.info>
2012-12-27 09:13:52 -0500

    Testing with a bigger blob in chunks.

--- test/coffer_simple_storage_test.erl
+++ test/coffer_simple_storage_test.erl
@@ -9,7 +9,10 @@
 
 basic_files_test_() ->
 	[{"Testing with simple files first.",
-	 ?setup(fun store_and_retrieve_a_file/1)}].
+	 ?setup(fun store_and_retrieve_a_file/1)},
+	 {"Testing with a big file.",
+	 ?setup(fun store_and_retrieve_a_big_file/1)}
+	].
 
 %%%%%%%%%%%%%%%%%%%%%%%
 %%% SETUP FUNCTIONS %%%
@@ -35,6 +38,40 @@ store_and_retrieve_a_file(_) ->
 	C2 = coffer_simple_storage:get_blob_content(ContentHash),
 	
 	[?_assert(ok =:= Res), ?_assert({ok, Content} =:= C2)].
+
+store_and_retrieve_a_big_file(_) ->
+	{ok, ContentBit} = file:read_file("/etc/passwd"),
+	Id = "1234567890",
+
+	% writing many "bits"
+	{ok, Ref} = coffer_simple_storage:store_blob_init(Id),
+	Res = store_loop(Ref, ContentBit, 0),
+
+	Size = size(ContentBit),
+	ExpectedFinalSize = 1000 * Size,
+
+	{ok, Ref2} = coffer_simple_storage:get_blob_init(Id),
+	ActualSize = compute_size(Ref2, 0),
+
+	[?_assert(ok =:= Res),
+	 ?_assert(ExpectedFinalSize =:= ActualSize)].
+
+store_loop(Ref, _ContentBit, 1000) ->
+	coffer_simple_storage:store_blob_end(Ref);
+store_loop(Ref, ContentBit, N) ->
+	coffer_simple_storage:store_blob(Ref, ContentBit),
+	store_loop(Ref, ContentBit, N+1).
+
+compute_size(Ref, N) ->
+	case coffer_simple_storage:get_blob(Ref) of
+		eof ->
+			N;
+		{ok, Data} ->
+			CurrentSize = size(Data),
+			compute_size(Ref, N + CurrentSize);
+		Other ->
+			Other
+	end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%
 %%% HELPER FUNCTIONS %%%
