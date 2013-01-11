30279c35d2b23450289f299402bc05899e53dd11
Nicolas R Dufour <nicolas.dufour@nemoworld.info>
2013-01-11 13:56:17 -0500

    Working POST using multipart and removing jiffy for now (conflict with lager to solve later).

--- cofferd/src/cofferd_main_handler.erl
+++ cofferd/src/cofferd_main_handler.erl
@@ -25,23 +25,9 @@ maybe_process_it(<<"GET">>, false, Req0) ->
             cowboy_req:reply(404, [], <<"Doesn't exist">>, Req)
     end;
 
-maybe_process_it(<<"POST">>, true, Req0) ->
-    {[ContentId], Req} = cowboy_req:path_info(Req0),
-    case cowboy_req:has_body(Req) of
-        {true, Req2} ->
-            {ok, Token} = cofferd_manager:store_blob_init(ContentId),
-            case iterate_over_writing_chunks(Req2, Token, 0) of
-                {ok, Req3} ->
-                    cowboy_req:reply(201, [], <<"Done">>, Req3);
-                _Other ->
-                    cowboy_req:reply(400, [], <<"Error...">>, Req2)
-            end;
-        {false, Req2} ->
-            cowboy_req:reply(400, [], <<"Bad request">>, Req2)
-    end;
-
-maybe_process_it(<<"POST">>, false, Req) ->
-    cowboy_req:reply(400, [], <<"bad request, check input data">>, Req);
+maybe_process_it(<<"POST">>, true, Req) ->
+    {_, Req2} = iterate_over_parts_with(Req),
+    cowboy_req:reply(201, [], <<"OK">>, Req2);
 
 maybe_process_it(<<"DELETE">>, false, Req0) ->
     {[ContentId], Req} = cowboy_req:path_info(Req0),
@@ -74,34 +60,55 @@ iterate_over_reading_chunks(Req, Token) ->
             {ok, Req}
     end.
 
-iterate_over_writing_chunks(Req, Token, FinalSize) ->
-    case cowboy_req:stream_body(Req) of
-        {ok, Data, Req2} ->
-            %io:format("FinalSize: ~p~n", [FinalSize]),
-            %%io:format("DATA: {~p}~n", [Data]),
-            cofferd_manager:store_blob(Token, Data),
-            iterate_over_writing_chunks(Req2, Token, FinalSize+size(Data));
-        {done, Req2} ->
-            cofferd_manager:store_blob_end(Token),
-            io:format("Final size: ~p~n", [FinalSize]),
-            {ok, Req2};
-        {error, Reason} ->
-            lager:error("An error occured during writing a blob: ~p", [Reason]),
-            {error, Reason}
-    end.
+%iterate_over_writing_chunks(Req, Token, FinalSize) ->
+%    case cowboy_req:stream_body(Req) of
+%        {ok, Data, Req2} ->
+%            %io:format("FinalSize: ~p~n", [FinalSize]),
+%            %%io:format("DATA: {~p}~n", [Data]),
+%            cofferd_manager:store_blob(Token, Data),
+%            iterate_over_writing_chunks(Req2, Token, FinalSize+size(Data));
+%        {done, Req2} ->
+%            cofferd_manager:store_blob_end(Token),
+%            io:format("Final size: ~p~n", [FinalSize]),
+%            {ok, Req2};
+%        {error, Reason} ->
+%            lager:error("An error occured during writing a blob: ~p", [Reason]),
+%%            {error, Reason}
+%    end.
 
 %%
 
-% acc_multipart(Req) ->
-%     acc_multipart(cowboy_req:multipart_data(Req), []).
+iterate_over_parts_with(Req) ->
+    iterate_over_parts(cowboy_req:multipart_data(Req), <<>>).
 
-% acc_multipart({headers, Headers, Req}, Acc) ->
-%     lager:info("HEADERS ARE: ~p~n", [Headers]),
-%     acc_multipart(cowboy_req:multipart_data(Req), [{Headers, []}|Acc]);
-% acc_multipart({body, Data, Req}, [{Headers, BodyAcc}|Acc]) ->
-%     acc_multipart(cowboy_req:multipart_data(Req), [{Headers, [Data|BodyAcc]}|Acc]);
-% acc_multipart({end_of_part, Req}, [{Headers, BodyAcc}|Acc]) ->
-%     acc_multipart(cowboy_req:multipart_data(Req),
-%         [{Headers, list_to_binary(lists:reverse(BodyAcc))}|Acc]);
-% acc_multipart({eof, Req}, Acc) ->
-%     {lists:reverse(Acc), Req}.
+iterate_over_parts({headers, Headers, Req}, _) ->
+    Comp = proplists:get_value(<<"content-disposition">>, Headers),
+
+    % TODO: mimetype should be stored as a metadata
+    %Mtype = proplists:get_value(<<"content-type">>, Headers),
+
+    SomeFields = binary:split(Comp, <<"; ">>, [global]),
+    Fields = lists:foldl(
+        fun(X, Acc1) ->
+            case binary:split(X, <<"=">>) of
+                [_] -> Acc1;
+                [Key, Value] -> [{Key, binary:replace(Value, <<"\"">>, <<>>, [global])} | Acc1]
+            end
+        end,
+        [],
+        SomeFields
+    ),
+
+    ContentId = proplists:get_value(<<"name">>, Fields),
+
+    {ok, Token} = cofferd_manager:store_blob_init(ContentId),
+
+    iterate_over_parts(cowboy_req:multipart_data(Req), Token);
+iterate_over_parts({body, Data, Req}, Token) ->
+    cofferd_manager:store_blob(Token, Data),
+    iterate_over_parts(cowboy_req:multipart_data(Req), Token);
+iterate_over_parts({end_of_part, Req}, Token) ->
+    cofferd_manager:store_blob_end(Token),
+    iterate_over_parts(cowboy_req:multipart_data(Req), <<>>);
+iterate_over_parts({eof, Req}, _) ->
+    {ok, Req}.
